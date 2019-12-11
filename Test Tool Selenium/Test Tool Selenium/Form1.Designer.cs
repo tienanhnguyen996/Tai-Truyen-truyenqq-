7ff1d24c7c4a5396e4e3326be281a99c703130b8
unknown <tienanhnguyen996@gmail.com>
2019-12-11 13:10:14 +0700

    40minute

---
+++ Test Tool Selenium/Test Tool Selenium/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Test_Tool_Selenium
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.path = new System.Windows.Forms.TextBox();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.link = new System.Windows.Forms.TextBox();
+            this.soLuong = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(13, 29);
+            this.path.Name = "path";
+            this.path.ReadOnly = true;
+            this.path.Size = new System.Drawing.Size(355, 20);
+            this.path.TabIndex = 0;
+            this.path.Text = "C:\\";
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(374, 27);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(75, 23);
+            this.browseBtn.TabIndex = 1;
+            this.browseBtn.Text = "Browse..";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nơi Lưu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Link Chap Bắt Đầu";
+            // 
+            // link
+            // 
+            this.link.Location = new System.Drawing.Point(12, 72);
+            this.link.Name = "link";
+            this.link.Size = new System.Drawing.Size(356, 20);
+            this.link.TabIndex = 4;
+            // 
+            // soLuong
+            // 
+            this.soLuong.Location = new System.Drawing.Point(375, 71);
+            this.soLuong.Name = "soLuong";
+            this.soLuong.Size = new System.Drawing.Size(74, 20);
+            this.soLuong.TabIndex = 5;
+            this.soLuong.TextChanged += new System.EventHandler(this.soLuong_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(374, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Số Lượng Chap ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(177, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Bắt Đầu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 130);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.soLuong);
+            this.Controls.Add(this.link);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.path);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox link;
+        private System.Windows.Forms.TextBox soLuong;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Test Tool Selenium/Test Tool Selenium/Form1.cs
@@ -0,0 +1,267 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Test_Tool_Selenium
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.ShowNewFolderButton = true;
+            DialogResult dialogResult = folder.ShowDialog();
+            if(dialogResult == DialogResult.OK)
+            {
+                path.Text = folder.SelectedPath;
+            }
+        }
+
+        ChromeDriver chrome;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new Thread(() => {
+                link.Enabled = false;
+                button2.Enabled = false;
+                soLuong.Enabled = false;
+                if (link.Text.Equals(""))
+                {
+                    MessageBox.Show("Vui lòng nhập link");
+                }
+                else
+                {
+                    if (soLuong.Text.Equals(""))
+                    {
+                        MessageBox.Show("Vui lòng nhập số lượng chap cần tải");
+                    }
+                    else
+                    {
+                        chrome = new ChromeDriver();
+                        chrome.Url = link.Text;
+                        chrome.Navigate();
+                        for (int i = 0; i < Int16.Parse(soLuong.Text); i++)
+                        {
+                            WaitToLoad(chrome, By.ClassName("lazy"));
+                            chrome.ExecuteScript("console.log('Load Page Done');");
+                            createDirectory(chrome);
+                            getImage(chrome, i);
+                            string size = "\\" + xuli(chrome.FindElementByClassName("detail-title").Text);
+                            path.Text = path.Text.Substring(0, path.Text.Length - size.Length);
+                            try
+                            {
+                                chrome.Url = chrome.FindElementByClassName("link-next-chap").GetAttribute("href");
+                                chrome.Navigate();
+                            }
+                            catch(Exception)
+                            {
+                                MessageBox.Show("Không Tìm Thấy Link Chap Mới. Kêt Thúc Việc Tải");
+                                break;
+                            }
+                        }
+                        chrome.Close();
+                        chrome.Quit();
+                    }
+                }
+                link.Enabled = true;
+                button2.Enabled = true;
+                soLuong.Enabled = true;
+                MessageBox.Show("Done");
+            })
+            { IsBackground = true }.Start();    
+        }
+
+        private string xuli(string title)
+        {
+            title = title.Replace(':', ' ');
+            title = title.Replace('<', ' ');
+            title = title.Replace('>', ' ');
+            title = title.Replace('?', ' ');
+            title = title.Replace('*', ' ');
+            title = title.Replace('/', ' ');
+            title = title.Replace('\\', ' ');
+            title = title.Replace('|', ' ');
+            return title;
+        }
+
+        private void createDirectory(ChromeDriver chrome)
+        {
+            string dirPath = path.Text = path.Text+ "\\" + xuli(chrome.FindElementByClassName("detail-title").Text);
+            bool exist = Directory.Exists(dirPath);
+            // Nếu không tồn tại, tạo thư mục này.
+            if (!exist)
+            {
+                // Tạo thư mục.
+                Directory.CreateDirectory(dirPath);
+            }
+            else
+            {
+            }
+        }
+
+        private void getImage(ChromeDriver chrome,int a)
+        {
+            var img = chrome.FindElementsByClassName("lazy");
+            for(int i=0;i<img.Count;i++)
+            {
+                DowloadImage(img[i].GetAttribute("src"), i);   
+            }
+        }
+
+        public void DowloadImage(string url,int i)
+        {
+            new Thread(() => {
+                try
+                {
+                    SaveImage(url, path.Text + "/" + i.ToString() + ".png", ImageFormat.Png);
+                }
+                catch (ExternalException)
+                {
+                    // Something is wrong with Format -- Maybe required Format is not 
+                    // applicable here
+                }
+                catch (ArgumentNullException)
+                {
+                    // Something wrong with Stream
+                }
+            }){ IsBackground = true }.Start();
+        }
+
+        public void SaveImage(string imageUrl,string filename, ImageFormat format)
+        {
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead(imageUrl);
+            Bitmap bitmap; bitmap = new Bitmap(stream);
+
+            if (bitmap != null)
+            {
+                bitmap.Save(filename, format);
+            }
+
+            stream.Flush();
+            stream.Close();
+            client.Dispose();
+        }
+
+        private void soLuong_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(soLuong.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Vui lòng chỉ nhập số.");
+                soLuong.Text = soLuong.Text.Remove(soLuong.Text.Length - 1);
+            }
+        }
+
+        public bool WaitToLoad(ChromeDriver chrome,By by)
+        {
+            int i = 0;
+            while (i < 600)
+            {
+                i++;
+                Thread.Sleep(100); // sleep 100 ms
+                try
+                {
+                    chrome.FindElement(by);
+                    break;
+                }
+                catch { }
+            }
+            if (i == 600) return false; // page load failed in 1 min
+            else return true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+        /*
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChromeDriver chromeDriver = new ChromeDriver();
+            chromeDriver.Url = "https://www.instagram.com/?hl=vi";
+            chromeDriver.Navigate();
+            Thread thread = new Thread(() =>
+            {
+                chromeDriver.Manage().Window.Maximize();
+                Thread.Sleep(1000);
+                chromeDriver.ExecuteScript("document.getElementsByClassName('sqdOP L3NKy   y3zKF')[0].click();");
+                loginfb(chromeDriver);
+                Thread.Sleep(1000);
+                chromeDriver.Url = "https://golike.site/login";
+                Login(chromeDriver);
+                Thread.Sleep(500);
+                chromeDriver.Url = "https://golike.site/jobs/instagram";
+                Thread.Sleep(1000);
+                chromeDriver.ExecuteScript("document.getElementsByClassName('col text-right')[0].click();");
+                Thread.Sleep(1000);
+                //chromeDriver.ExecuteScript("document.getElementsByClassName('btn bg-button - 1 px - 0 btn - block')[0].click();");
+                MessageBox.Show(getJob(chromeDriver));
+
+            });
+            thread.IsBackground = true;
+            thread.Start();
+        }
+        private bool IsElementPresent(By by, ChromeDriver driver)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        private void Login(ChromeDriver chromeDriver)
+        {
+            var input = chromeDriver.FindElementsByClassName("form-control");
+            input[0].SendKeys("tienanh511");
+            input[1].SendKeys("Nguyen123");
+            chromeDriver.ExecuteScript("document.getElementsByClassName('btn bg-gradient-1 py-2 border-0 text-light btn-block')[0].click();");
+        }
+        private void loginfb(ChromeDriver chromeDriver)
+        {
+            var email = chromeDriver.FindElementByName("email");
+            email.SendKeys("0932563378");
+            var pass = chromeDriver.FindElementByName("pass");
+            pass.SendKeys("Matkhau123");
+            if(IsElementPresent(By.Name("login"),chromeDriver))
+            {
+                var submit = chromeDriver.FindElementByName("login");
+                submit.Click();
+            }
+            else
+            {
+                var login = chromeDriver.FindElementById("loginbutton");
+                login.Click();
+            }
+            //chromeDriver.ExecuteScript("document.getElementsByClassName('sqdOP  L3NKy   y3zKF     ')[0].click();");
+        }
+        
+        private string getJob(ChromeDriver chromeDriver)
+        {
+            IJavaScriptExecutor js = chromeDriver as IJavaScriptExecutor;
+            var a = (string)js.ExecuteScript("var a = document.getElementsByClassName('ml-1 d400')[0].innerText;return a;");
+            if(a == "")
+            {
+                MessageBox.Show("");
+            }
+            return a;
+        }*/
+    }
+}
